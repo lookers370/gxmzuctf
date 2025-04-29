4f08204c5351d1b6009a34dc35527ada94418718
lookers370 <lookers370@gmail.com>
2025-04-29 22:43:26 +0800

    Add files via upload

---
+++ post/src/index.php
@@ -0,0 +1,17 @@
+<pre>
+
+      ____ _____ ____              
+     / ___|_   _|  __| 
+    | |     | | | |__
+    | |___  | | |  __|
+     \____| |_| |_|  
+
+
+</pre>
+
+<h3> flag is in /flag.txt </h3>
+<h3> tip!!!!--->file </h3>
+<?php
+$file = isset($_POST['file']);
+include($file);
+?>
